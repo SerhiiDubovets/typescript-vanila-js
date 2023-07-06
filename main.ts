66a81f144d88a3a312f44b80f4948afc6a451ebf
SerhiiDubovets <Sergio.d@bigmir.net>
2023-07-06 13:43:45 +0300

    function

--- main.ts
+++ main.ts
@@ -1,6 +1,28 @@
-//картеж
+function getChannel(name:string):{name:string}{
+return {name}
+}
 
-let fixed: [string, number, null];
+getChannel("Bob")
 
-fixed = ['Text', 10, null];
+//arrow function
+const getChannelName=(name:string):{name:string}=>{
+   return {name}
+};
+getChannelName("Bill")
 
+ //with type
+ type TypeChannelReturn = {name:string}
+ type TypeChannelName = (name: string) => TypeChannelReturn;
+ const getChannelName2:TypeChannelName=(name)=>{
+   return {name}
+};
+ 
+//функциональные перегрузки
+function getCar(name:string):string;
+function getCar(name:string, price:number):string;
+function getCar(name:string, price?: number):string{
+   return price ? `Name ${name}` : `name ${name}`
+}
+
+const car1 = getCar("bmw");
+const car2 = getCar("bmw", 100000);
