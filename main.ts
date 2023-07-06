cccca0d486b2356ace7fdf60a9a6428cdc983961
SerhiiDubovets <Sergio.d@bigmir.net>
2023-07-06 12:13:00 +0300

    base types

--- main.ts
+++ main.ts
@@ -1,2 +1,26 @@
-let firstName: string =  "";
+//string
+let firstName: string;
 firstName = "Bob"
+
+//number
+let num: number;
+num = 10
+
+//boolean
+let bool: boolean;
+bool = true
+
+//undefined
+let undef: undefined;
+
+//null
+let nul: null;
+
+//any
+let anyValue: any;
+
+//unknown
+let unknownValue: unknown;
+
+//void
+let voidValue: void;
